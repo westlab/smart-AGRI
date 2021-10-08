a425aabda2d36fdbb5e47b8ef169d36ab16c96aa
hiroki0505 <hiroki.jaakaku@gmail.com>
2021-10-08 18:30:25 +0900

    クライアントID,シークレットを登録するだけで勝手にAPIをとってくるように変更しました

--- weather.py
+++ weather.py
@@ -1,9 +1,20 @@
 import requests
 import json
-headers = {'X-Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJBY2Nlc3NUb2tlbklkIjoiMzIwM2I5NDgtZDg0YS00NmE5LWIxNzItYjVhYzA3YzMyZTk2IiwiaXNzIjoiaHR0cHM6Ly9hcGkud2FncmkubmV0IiwiYXVkIjoiaHR0cHM6Ly9hcGkud2FncmkubmV0IiwiZXhwIjoxNjMxNjI5MDk2LCJuYmYiOjE2MzE1ODU4OTZ9.J4cmVB7_foVfmFSflXpFDqXBGjrkQuEuB8DXBORdgR8'}
-response = requests.get('https://api.wagri.net/API/Public/Weather/Forecast',headers=headers)
-##print(response.status_code)    # HTTPのステータスコード取得
+import pandas as pd
+from pandas.io.json import json_normalize
+
+
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+body = {'grant_type':'client_credentials','client_id':'a04d9a5c-fb87-4c1b-91ea-4edce22c1592','client_secret':'210oumn=ie'}
+response = requests.post('https://api.wagri.net/Token',headers=headers,data=body)
 response.headers = {"Content-Type": "application/json; charset=utf-8"}
 data = response.json()
-##print (json.dumps(data, indent=4))    ##これは日本語文字化けする
-print (response.text)   ##レスポンスを全てテキストで出力
+access_token = data["access_token"]
+headers = {'X-Authorization': access_token}
+response = requests.get('https://api.wagri.net/API/Public/Weather/Forecast',headers=headers)
+print(response.status_code)    # HTTPのステータスコード取得
+response.headers = {"Content-Type": "application/json; charset=utf-8"}
+data = response.json()
+print(data)
+##data = json.dumps(data, indent=4) ##これは日本語文字化けする
+##print (response.text[:300])   ##レスポンスを全てテキストで出力
